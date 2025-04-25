dff1fe6ae81793842155d7213c2a01608835aae8
nqianhuiii <nghui@graduate.utm.my>
2025-04-25 17:57:53 +0800

    add those config

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { withNativeWind } = require('nativewind/metro');
   
 const config = getDefaultConfig(__dirname);
   
+config.resolver.alias = {
+    "@": require("path").resolve(__dirname, "src"),
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
